833e0d5896c8539e7568decfcf578c491472c7e3
admin <manikantasama4@outlook.com>
2022-08-06 15:34:53 -0400

    added the html files, added dependencies

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,7 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Todo}/{action=Index}/{id?}");
 
 app.Run();
-
